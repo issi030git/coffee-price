703b09f034d76ac077f41ec9377688fdaf8a5ecc
issi8131 <shochioa@gmail.com>
2025-03-05 20:31:54 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,62 +1,60 @@
-const GEO_API_URL = "https://geoapi.heartrails.com/api/json?method=searchByGeoLocation";
-
 var app = new Vue({
   el: '#app',
   data: {
-    tax_flag: false,
+    //timer関係
+    status: "clear",
+    time: 0,
+    startTime: null,
+    stopTime: 0,
+
+    //画面遷移関係
+    activeScreenNo: 1,
+
+    //ドロワー関係
     drawerVisible: false,
     direction: 'rtl',//ドロワーの方向
     width: window.innerWidth,
-    detailMode: false,
-    now_address: "---",
-    now_lat: 0,
-    now_lng: 0,
-    items: [
+
+    //設定関係
+    selectedSettingNo: 0,
+    settings: [
       {
-        store_name: "スターバックスコーヒー",
-        price: 290,
-        icons: [],
+        settingName: "4:6メソッド20g300ml",
+        pairs: [
+          { startTime: 0, targetAmount: 60 },
+          { startTime: 40, targetAmount: 120 },
+          { startTime: 90, targetAmount: 180 },
+          { startTime: 130, targetAmount: 240 },
+          { startTime: 160, targetAmount: 300 },
+        ]
       },
       {
-        store_name: "ドトールコーヒー",
-        price: 220,
-        icons: [],
+        settingName: "4:6メソッド14g200ml",
+        pairs: [
+          { startTime: 0, targetAmount: 40 },
+          { startTime: 40, targetAmount: 80 },
+          { startTime: 90, targetAmount: 120 },
+          { startTime: 130, targetAmount: 160 },
+          { startTime: 160, targetAmount: 200 },
+        ]
       },
       {
-        store_name: "星乃珈琲店",
-        price: 420,
-        icons: [],
-      },
-      {
-        store_name: "コメダ珈琲店",
-        price: 510,
-        icons: [],
-      },
-      {
-        store_name: "タリーズコーヒー",
-        price: 300,
-        icons: [],
-      },
-      {
-        store_name: "サンマルクカフェ",
-        price: 200,
-        icons: [],
+        settingName: "テストデータ",
+        pairs: [
+          { startTime: 0, targetAmount: 10 },
+          { startTime: 5, targetAmount: 20 },
+          { startTime: 10, targetAmount: 30 },
+        ]
       },
     ],
   },
 
   filters: {
-    taxin: function (value) {
-      return parseInt(value * 1.1);
-    }
   },
 
   computed: {// 何か処理をした結果をデータとして返す
-    // priceで降順並べ替え
-    sortedItemsByPrice() {
-      return this.items.sort((a, b) => {
-        return (a.price < b.price) ? 1 : (a.price > b.price) ? -1 : 0;
-      });;
+    getTimerSecondValue: function () {
+      return Math.floor(this.time / 1000)
     },
     drawerWidth: function () {//ページ表示時のウィンドウ幅に応じてダイアログのwidthを計算
       if (this.width < 640)
@@ -67,62 +65,75 @@ var app = new Vue({
   },
 
   methods: {
-    openMap: function (store_name) {
-      if (!navigator.geolocation) {
-        // Geolocation APIに対応していない
-        alert("端末が未対応です");
-        return;
+    //現在のカウント値に基づき各抽出量行の色を変えるためのクラス名を返す
+    getRowColor(idx) {
+      let row_pairs = this.settings[this.selectedSettingNo].pairs;
+      let s_time = row_pairs[idx].startTime;
+      let next_row_time = row_pairs.length <= idx + 1 ? 999 : row_pairs[idx + 1].startTime;
+
+      if (this.status !== "start")
+        return { "normal-row": true }
+
+      if (s_time <= this.getTimerSecondValue && this.getTimerSecondValue < next_row_time)
+        return { "active-row": true }
+      else if (this.getTimerSecondValue < s_time)
+        return { "normal-row": true }
+      else
+        return { "inactive-row": true }
+    },
+
+    start() {
+      this.status = "start"
+
+      if (this.startTime === null) {
+        this.startTime = Date.now()
       }
-      // 現在地を取得
-      navigator.geolocation.getCurrentPosition(
-        // 取得成功した場合
-        position => {
-          var crd = position.coords;
-          this.now_lat = crd.latitude;//緯度
-          this.now_lng = crd.longitude;//経度
 
-          axios
-            .get(GEO_API_URL + "&" + "x=" + this.now_lng + "&" + "y=" + this.now_lat)//xは経度、yは緯度
-            .then(response => {//緯度経度からの地名取得に成功したら
-              let loc = response.data;
+      const checkTime = () => {
+        this.time = Date.now() - this.startTime + this.stopTime
+      }
+      this.timer = setInterval(checkTime, 10);
+    },
+    stop() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
 
-              this.now_address = "";
-              try {
-                loc = loc.response.location[0];
-                this.now_address = loc.city + "+" + loc.town;//〇〇市△△3丁目、といった感じの文字列
-              } catch (error) { }
+      this.status = "stop"
+      this.startTime = null
+      this.stopTime = this.time
+    },
+    reset() {
+      this.stop()
+      this.status = "clear"
+      this.time = 0
+      this.startTime = null
+      this.stopTime = 0
+    },
+    getTimeStr() {
+      // this.time is milliseconds
+      // 1秒 = 1000ミリ秒
+      // 1分 = 60 * 1000ミリ秒
+      // 1時間 = 60 * 60 * 1000ミリ秒
+      let milliseconds = this.time % 1000
+      let seconds = Math.floor((this.time / 1000) % 60)
+      let minutes = Math.floor((this.time / (60 * 1000)) % 60)
+      //let hours = Math.floor(this.time / (60 * 60 * 1000))
 
-              let url = "https://www.google.com/maps/search/?api=1&query=" + this.now_address + "+" + store_name;
-              window.location.href = url; // 遷移
-            })
-            .catch(function (error) {
-              alert('現在位置の地名取得でエラーが発生しました')
-            });
-        },
-        // 取得失敗した場合
-        function (error) {
-          switch (error.code) {
-            case 1: //PERMISSION_DENIED
-              alert("位置情報の利用が許可されていません");
-              break;
-            case 2: //POSITION_UNAVAILABLE
-              alert("現在位置が取得できませんでした");
-              break;
-            case 3: //TIMEOUT
-              alert("タイムアウトになりました");
-              break;
-            default:
-              alert("その他のエラー(エラーコード:" + error.code + ")");
-              break;
-          }
-        });
+      let millisecondsMultiplyTen = Math.floor(milliseconds / 10)
+
+      millisecondsMultiplyTen = ("0" + millisecondsMultiplyTen).slice(-2)
+      seconds = ("0" + seconds).slice(-2)
+      minutes = ("0" + minutes).slice(-2)
+      //hours = hours < 100 ? ("0" + hours).slice(-2) : hours
+
+      return `${minutes}:${seconds}.${millisecondsMultiplyTen}`
     },
 
     handleResize: function () {
       // resizeのたびにやりたいことを記述
       this.width = window.innerWidth;
     },
-
   },
 
   mounted: function () {
